ee8efeb5d85ab886d1975084e1512555645c1e74
lucaschier <ens-lucasschier@ugv.edu.br>
2023-05-23 15:57:12 -0300

    novo update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,1 +1,10 @@
-console.log(" -*- Teste -*- ");
+import { execSync } from "child_process";
+
+console.log("[DEPLOY_PREVIEW]: START");
+const command = "yarn deploy:staging";
+const output = execSync(command, { encoding: "utf8" });
+console.log("[DEPLOY_PREVIEW]: END");
+console.log(output);
+
+//console.log("[GITHUB_COMMENT]: START");
+//console.log("[GITHUB_COMMENT]: END");
